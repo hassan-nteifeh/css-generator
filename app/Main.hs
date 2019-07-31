d0cd9056e748e8b1850e8b7895c3a0d638509a76
Hassan Nteifeh <hassan.nteifeh@outlook.com>
2019-07-31 21:52:33 +0200

    2nd commit

--- app/Main.hs
+++ app/Main.hs
@@ -2,30 +2,25 @@
 module Main where 
 
 import qualified Data.ByteString.Lazy as Lz
-import qualified Data.ByteString as B
+import Numeric (showFFloat)
 import GHC.Exts    -- (fromList)
 import Data.Typeable
 import Data.Aeson (encode, eitherDecode)
 import Data.Aeson.Types
 import Domain (Config, generateOpaClasses, opacity, colors, processColorData, generateColorsDeclarations)
-import qualified Data.Text.Lazy.IO as T
-import qualified Data.Text.Lazy.Encoding as T
-import qualified Data.HashMap.Lazy as Map
+import qualified Data.Text as T
+import qualified Data.Text.IO as T
+import qualified Data.Text.Encoding as T
+import qualified Data.HashMap.Strict as HM
 
-import GHC.Exts    -- (fromList)
-
-val :: Value
-val = Object $ fromList [
-  ("numbers", Array $ fromList [Number 1, Number 2, Number 3]),
-  ("boolean", Bool True) ]
 
 main :: IO ()
 main = do
     d <- eitherDecode <$> (Lz.readFile "./config.json") :: IO (Either String Config)
-    T.putStrLn . T.decodeUtf8 . encode $ val
+    putStrLn $ showFullPrecision (0.0005 :: Float)
     case d of
         Left err -> putStrLn err
         Right ps -> do
             putStrLn $ show $ length $ toList $ colors ps
             --T.writeFile "test.css" $ generateColorsDeclarations $ processColorData $ toList $ colors ps
-            putStrLn $ generateOpaClasses $ opacity ps
+            T.writeFile "test.css"  $ T.pack $ generateOpaClasses $ opacity ps
--- src/Domain.hs
+++ src/Domain.hs
@@ -4,7 +4,9 @@ module Domain where
 
 import Data.Aeson
 import GHC.Generics
+import Numeric (showFFloat)
 import Data.Typeable
+import qualified Data.Scientific as DS
 import qualified Data.Text as T
 import qualified Data.HashMap.Lazy as Map
 import GHC.Exts    -- (fromList)
@@ -12,16 +14,22 @@ import GHC.Exts    -- (fromList)
 unwrap :: Value -> T.Text
 unwrap (String val) = val
 
-createOpaClass :: Double -> [Char]
-createOpaClass x = case x of
-    0.0 -> ".op-0 {\n" ++ "opacity: " ++ show x ++ ";\n}"
-    1.0 -> ".op-1 {\n" ++ "opacity: " ++ show x ++ ";\n}"
-    _ -> let name = filter (\c -> c /= '.') $ "op-" ++ (show x)
-      in "." ++ name ++ "{\n" ++ "opacity: " ++ show x ++ ";\n}"
+showFullPrecision :: Float -> String
+showFullPrecision x = showFFloat Nothing x ""
 
-generateOpaClasses :: [Double] -> [Char] 
+createOpaClass :: Float -> [Char]
+createOpaClass x = case x of
+    0.0 -> ".o-0 {\n" ++ "opacity: " ++ showFullPrecision x ++ ";\n}"
+    1.0 -> ".o-100 {\n" ++ "opacity: " ++ showFullPrecision x ++ ";\n}"
+    _ -> let name = filter (\c -> c /= '.') $ "o-" ++ (showFullPrecision (x * 10))
+      in "." ++ name ++ "{\n" ++ "opacity: " ++ showFullPrecision x ++ ";\n}"
+
+generateOpaClasses :: [Float] -> [Char] 
 generateOpaClasses = 
-    foldl (\acc x -> acc ++ "\n\n" ++ (createOpaClass x)) ""
+    foldl (\acc x -> case acc of
+        "" -> acc ++ (createOpaClass x)
+        _ -> acc ++ ("\n\n") ++ (createOpaClass x)) 
+        ""
 
 processColorData x =  map (\v -> (fst v, unwrap $ snd $ v)) x
 
@@ -33,7 +41,7 @@ generateColorsDeclarations ls = foldl (\ acc val -> case val of
     ls
     
 data Config = Config {
-    opacity :: [Double],
+    opacity :: [Float],
     colors :: Object
 } deriving (Show)
 
